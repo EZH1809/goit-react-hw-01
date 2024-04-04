c426d3355f3bec06cc8a9271c2ec33431fc986d5
EZH1809 <stellage.ua@gmail.com>
2024-04-04 19:05:51 +0300

    add TransactionHistory

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -2,7 +2,8 @@ import Profile from "../Profile/Profile"
 import userData from "../Profile/userData.json";
 import FriendList from "../FriendList/FriendList";
 import friends from "../FriendList/friends.json";
-
+import TransactionHistory from "../TransactionHistory/TransactionHistory"
+import transactions from "../TransactionHistory/transactions.json";
 import style from "./App.module.css";
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
 
   );
--- src/component/TransactionHistory/TransactionHistory.jsx
+++ src/component/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,24 @@
-import css from "./TransactionHistory.module.css"
+import style from "./TransactionHistory.module.css"
 
 export default function TransactionHistory({ items }) {
     return (
-        <table className={css.table}>
-            <thead className={css.tableThread}>
+        <table className={style.table}>
+            <thead className={style.tableThread}>
                 <tr>
-                    <th className={css.tableThreadText}>Type</th>
-                    <th className={css.tableThreadText}>Amount</th>
-                    <th className={css.tableThreadText}>Currency</th>
+                    <th >Type</th>
+                    <th >Amount</th>
+                    <th >Currency</th>
                 </tr>
             </thead>
 
-            <tbody className={css.tableBody}>
-                {items.map((item) => {return <tr className={css.bodyTd} key={item.id}>
-                    <td className={css.tableThreadVal}>{item.type}</td>
-                    <td className={css.tableThreadVal}>{item.amount}</td>
-                    <td className={css.tableThreadVal}>{item.currency}</td>
-                </tr>})}
+            <tbody className={style.tableBody}>
+                {items.map(({ id, type, amount, currency }) => (
+                    <tr className={style.bodyTd} key={id}>
+                        <td className={style.tableThreadItem}>{type}</td>
+                        <td className={style.tableThreadItem}>{amount}</td>
+                        <td className={style.tableThreadItem}>{currency}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
